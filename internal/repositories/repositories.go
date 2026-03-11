a1bbcd1eff59f873b885ec6127e8dd3085e40791
a1bruno <brunotrdev@gmail.com>
2026-03-11 10:03:44 -0300

    feat: adiciona a interface do repository

--- internal/repositories/repositories.go
+++ internal/repositories/repositories.go
@@ -1,1 +1,18 @@
 package repositories
+
+import (
+	"github.com/a1bruno/order-api/internal/models"
+	"github.com/google/uuid"
+)
+
+type Repositories struct {
+	Order interface {
+		GetAll() []models.Order
+		Add(newUser models.Order)
+		Refund(id uuid.UUID)
+	}
+}
+
+func New() *Repositories {
+	return &Repositories{}
+}
