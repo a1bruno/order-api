676def1fa41acf5c2dea9d5efa3ffac079ec0fa6
a1bruno <brunotrdev@gmail.com>
2026-03-11 09:52:29 -0300

    feat: cria o modelo de dado dos pedidos

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/a1bruno/order-api
 
 go 1.25.6
+
+require github.com/google/uuid v1.6.0
--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,10 @@
 package models
+
+import "github.com/google/uuid"
+
+type Order struct {
+	ID       uuid.UUID
+	Customer string
+	Price    float64
+	Refunded bool
+}
