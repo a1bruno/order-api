16deb59982383abfdc7c98d3f1e9938c64eac688
a1bruno <brunotrdev@gmail.com>
2026-03-12 10:37:07 -0300

    fix: define uma nova struct para recebimento de dados do usuário

--- internal/models/models.go
+++ internal/models/models.go
@@ -8,3 +8,8 @@ type Order struct {
 	Price    float64
 	Refunded bool
 }
+
+type CreateOrder struct { //essa struct serve para implementar a lógica do Add() sem parametros extras do usuario
+	Customer string
+	Price    float64
+}
