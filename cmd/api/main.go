5bceb9a64a80b3f64e7396c972c66d4c7c5d1a42
a1bruno <brunotrdev@gmail.com>
2026-03-12 23:43:25 -0300

    feat: adiciona a injeção de dependências no main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,14 @@
-package api
+package main
 
-func main() {}
+import (
+	"github.com/a1bruno/order-api/internal/handlers"
+	"github.com/a1bruno/order-api/internal/repositories"
+	"github.com/a1bruno/order-api/internal/usecases"
+)
+
+func main() {
+	repos := repositories.New()
+	useCases := usecases.New(repos)
+	h := handlers.New(useCases)
+	h.Listen(8080)
+}
