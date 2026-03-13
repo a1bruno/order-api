1abe1d8767fee7c66149fee892193db75694f116
a1bruno <brunotrdev@gmail.com>
2026-03-12 23:37:17 -0300

    feat: implementa a primeira etapa da camada de handlers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,23 @@
 package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a1bruno/order-api/internal/usecases"
+)
+
+type Handlers struct {
+	useCases usecases.UseCases
+}
+
+func New(useCases *usecases.UseCases) *Handlers {
+	return &Handlers{}
+}
+
+func (h Handlers) Listen(port int) error {
+	return http.ListenAndServe(
+		fmt.Sprintf(":%v", port),
+		nil,
+	)
+}
