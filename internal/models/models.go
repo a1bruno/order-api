690bfa8e2c1e734abc79c2e00cdc6251dba1101a
a1bruno <brunotrdev@gmail.com>
2026-03-14 13:35:34 -0300

    fix: corrige a resposta das entradas da api no models

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,15 +3,15 @@ package models
 import "github.com/google/uuid"
 
 type Order struct {
-	ID       uuid.UUID
-	Customer string
-	Price    float64
-	Refunded bool
+	ID       uuid.UUID `json:"user_id"`
+	Customer string    `json:"customer_name"`
+	Price    float64   `json:"product_price"`
+	Refunded bool      `json:"refunded"`
 }
 
 type CreateOrder struct { //essa struct serve para implementar a lógica do Add() sem parametros extras do usuario
-	Customer string
-	Price    float64
+	Customer string  `json:"customer"`
+	Price    float64 `json:"price"`
 }
 
 type ResponseOrder struct {
