fc04ae700c42a533100ac0de178603897ab0bbc5
a1bruno <brunotrdev@gmail.com>
2026-04-23 18:51:49 -0300

    adiciona a propriedade para conectar no banco

--- internal/repositories/repositories.go
+++ internal/repositories/repositories.go
@@ -1,21 +1,25 @@
 package repositories
 
 import (
+	"database/sql"
+
 	"github.com/a1bruno/order-api/internal/models"
 	"github.com/a1bruno/order-api/internal/repositories/orders"
 	"github.com/google/uuid"
 )
 
 type Repositories struct {
-	Order interface {
+	connection *sql.DB
+	Order      interface {
 		GetAll() ([]models.Order, error)
 		Add(newUser models.Order) error
 		Refund(id uuid.UUID) error
 	}
 }
 
-func New() *Repositories {
+func New(connection *sql.DB) *Repositories {
 	return &Repositories{
-		Order: orders.New(),
+		connection: connection,
+		Order:      orders.New(),
 	}
 }
