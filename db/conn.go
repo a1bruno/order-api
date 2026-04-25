a481675d0388ee145b00149c01a90b2a37a986bd
a1bruno <brunotrdev@gmail.com>
2026-04-25 15:45:25 -0300

    refactor: define o uso das variaveis de ambiente antes da connection

--- db/conn.go
+++ db/conn.go
@@ -9,22 +9,16 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// var (
-// 	host     = os.Getenv("DB_HOST")
-// 	port     = os.Getenv("DB_PORT")
-// 	user     = os.Getenv("DB_USER")
-// 	password = os.Getenv("DB_PASSWORD")
-// 	dbname   = os.Getenv("DB_NAME")
-// )
-
 func NewDBConnection() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
 	connStr := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		host, port, user, password, dbname,
 	)
 
 	db, err := sql.Open("postgres", connStr)
